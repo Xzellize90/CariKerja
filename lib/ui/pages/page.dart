19c2229d1611274a5b9582f7c3cfd1b81f59b2b5
Xzellize90 <Xzellize90@gmail.com>
2020-12-12 16:54:20 +0800

    update codingan

--- lib/ui/pages/page.dart
+++ lib/ui/pages/page.dart
@@ -4,6 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
-part 'mainmenu.dart';
+part 'Amainmenu.dart';
 part 'signup_page.dart';
 part 'signin_page.dart';
