74e23dc23895341e9e3460a2fb1ce51d7fa725df
Xzellize90 <Xzellize90@gmail.com>
2020-12-12 16:35:26 +0800

    splashscreen

---
+++ lib/ui/pages/mainmenu.dart
@@ -0,0 +1,34 @@
+part of 'page.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 10,
+      backgroundColor: Colors.white,
+      image: Image.asset('assets/Double Ring-1s-200px.gif'),
+      loaderColor: Colors.white,
+      photoSize: 150.0,
+      navigateAfterSeconds: MainMenu(),
+    );
+  }
+}
+
+class MainMenu extends StatefulWidget {
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/ui/pages/page.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+part 'mainmenu.dart';
+part 'signup_page.dart';
+part 'signin_page.dart';
