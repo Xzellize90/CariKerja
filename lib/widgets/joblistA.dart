0e07c81bdce1aedb4be793bbbfdd6a4504975f0a
10101100011000 <55679377+10101100011000@users.noreply.github.com>
2021-01-03 15:42:33 +0700

    Fix_firebase_crash
    LOL

--- lib/widgets/joblistA.dart
+++ lib/widgets/joblistA.dart
@@ -1,9 +1,9 @@
 //import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:carikerja/models/modelsC.dart';
 import 'package:carikerja/ui/pages/pageA.dart';
-import 'package:carikerja/services/job_services.dart';
+//import 'package:carikerja/services/job_services.dart';
 
 import 'package:intl/intl.dart';
 
