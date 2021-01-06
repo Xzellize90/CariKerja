07ea49659d31cc3f87722224cbcc6a6665063cd5
MuhammadHakiki <56475762+MuhammadHakiki@users.noreply.github.com>
2021-01-06 22:02:44 +0700

    Apply_job

--- lib/ui/pages/Ajob.dart
+++ lib/ui/pages/Ajob.dart
@@ -12,6 +12,11 @@ class _JobAState extends State<JobA> {
   TextEditingController controllerPrice;
   CollectionReference userCollection =
       FirebaseFirestore.instance.collection("joblist");
+  User _auth = FirebaseAuth.instance.currentUser;
+
+  CollectionReference productCollection =
+      FirebaseFirestore.instance.collection("userA");
+
   String id,
       email,
       name,
@@ -25,13 +30,18 @@ class _JobAState extends State<JobA> {
       images;
 
   void getUserUpdate() async {
-    userCollection.doc().snapshots().listen((event) {
-      id = event.data()['judul'];
-      email = event.data()['deskripsi'];
-      images = event.data()['kontak'];
-      name = event.data()['gaji'];
-      lokai = event.data()['penempatan'];
-      ttl = event.data()['image'];
+    productCollection.doc(_auth.uid).snapshots().listen((event) {
+      id = event.data()['uid'];
+      email = event.data()['email'];
+      images = event.data()['profileApplicant'];
+      name = event.data()['namaA'];
+      lokai = event.data()['lokasi'];
+      ttl = event.data()['ttlahir'];
+      agama = event.data()['agama'];
+      hobby = event.data()['hobby'];
+      pendidikan = event.data()['spendidikan'];
+      skill = event.data()['skills'];
+      kerja = event.data()['pbekerja'];
 
       setState(() {});
     });
@@ -131,7 +141,34 @@ class _JobAState extends State<JobA> {
                           "Apply",
                           style: TextStyle(fontFamily: 'saira', fontSize: 30),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          ctrlId =
+                              TextEditingController(text: widget.joblist.id);
+                          FirebaseFirestore.instance
+                              .collection("joblist")
+                              .doc(widget.joblist.id)
+                              .collection("Appliance")
+                              .add({
+                            'appliance_id':
+                                FirebaseAuth.instance.currentUser.uid,
+                            'agama': agama ?? '',
+                            'email': email ?? '',
+                            'hobby': hobby ?? '',
+                            'lokasi': lokai ?? '',
+                            'namaA': name ?? '',
+                            'pbekerja': kerja ?? '',
+                            'skill': skill ?? '',
+                            'spendidikan': pendidikan ?? '',
+                            'ttl': ttl ?? ''
+                          });
+                          Fluttertoast.showToast(
+                              msg: "Add product successful.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              backgroundColor: Colors.green,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                        },
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(50)),
                       ),
