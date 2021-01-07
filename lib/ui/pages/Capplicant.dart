4f853db8213b9aee4f0fc1b801f3b607a3e32c16
MuhammadHakiki <56475762+MuhammadHakiki@users.noreply.github.com>
2021-01-07 16:47:28 +0700

    applicant_UI

--- lib/ui/pages/Capplicant.dart
+++ lib/ui/pages/Capplicant.dart
@@ -10,58 +10,68 @@ class Capplicant extends StatefulWidget {
 class _CapplicantState extends State<Capplicant> {
   @override
   Widget build(BuildContext context) {
-    print(widget.joblist.id);
     return Scaffold(
       backgroundColor: Color(0xFFEEA20F),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection("joblist")
-              .doc(widget.joblist.id)
-              .collection("Appliance")
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            print(snapshot);
-            if (snapshot.hasError) {
-              return Text("Failed to get products data!");
-            }
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("joblist")
+                .doc(widget.joblist.id)
+                .collection("Appliance")
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              print(context);
+              if (snapshot.hasError) {
+                return Text("Failed to get products data!");
+              }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return SpinKitFadingCircle(
-                size: 50,
-                color: Colors.blue,
-              );
-            }
-            if (snapshot.data == null) {
-              return Container(
-                child: Center(
-                  child: Text("Data Tidak Ada"),
-                ),
-              );
-            } else {
-              return ListView(
-                children: snapshot.data.docs.map((DocumentSnapshot doc) {
-                  return Applicantcard(
-                      user: UserA(
-                    doc.data()['id'],
-                    doc.data()['agama'],
-                    doc.data()['email'],
-                    doc.data()['hobby'],
-                    doc.data()['lokasi'],
-                    doc.data()['namaA'],
-                    doc.data()['pbekerja'],
-                    doc.data()['skills'],
-                    doc.data()['spendidikan'],
-                    doc.data()['ttlahir'],
-                    doc.data()['profileApplicant'],
-                  ));
-                }).toList(),
-              );
-            }
-          },
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return SpinKitFadingCircle(
+                  size: 50,
+                  color: Colors.blue,
+                );
+              }
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: Text("Data Tidak Ada"),
+                  ),
+                );
+              } else {
+                Center(
+                    child: Container(
+                  child: Text("Gambar Profile",
+                      style:
+                          TextStyle(color: Colors.white, fontFamily: 'saira')),
+                ));
+                return Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: ListView(
+                    children: snapshot.data.docs.map((DocumentSnapshot doc) {
+                      return Applicantcard(
+                          user: UserA(
+                        doc.data()['id'],
+                        doc.data()['agama'],
+                        doc.data()['email'],
+                        doc.data()['hobby'],
+                        doc.data()['lokasi'],
+                        doc.data()['namaA'],
+                        doc.data()['pbekerja'],
+                        doc.data()['skills'],
+                        doc.data()['spendidikan'],
+                        doc.data()['ttlahir'],
+                        doc.data()['profileApplicant'],
+                      ));
+                    }).toList(),
+                  ),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
